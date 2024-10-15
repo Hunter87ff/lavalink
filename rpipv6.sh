de0d495885290d37427aa3ff4ca3ffd8aca0df98
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-10-16 00:39:40 +0530

    Update rpipv6.sh

--- rpipv6.sh
+++ rpipv6.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-# -----
-# Name: RPIPv6
-# Copyright: 
-#  (c) 2016-2021 Michael Schneider (scip AG)
-#  (c) 2021 BackInBash
-# Date: 21-08-2021
-# Version: 0.0.1
-# -----
-
-# -----
-# Variables
-# -----
 count=0
 cmd_ip="/sbin/ip"
 cmd_awk="/usr/bin/awk"
 cmd_head="/usr/bin/head"
 squid_confd="/etc/squid/conf.d/"
 interface="eth0"
-network=$($cmd_ip -o -f inet6 addr show dev $interface | $cmd_awk '/scope global/ {print $4}' | $cmd_head -c -24)
-
 sleeptime="30s"
+
+# Get IPv6 network prefix
+network=$($cmd_ip -o -f inet6 addr show dev $interface | $cmd_awk '/scope global/ {print $4}')
+if [ -z "$network" ]; then
+  echo "No global IPv6 network found on $interface"
+  exit 1
+fi
 echo "Network: $network"
 
-# -----
+# Create squid configuration directory if it doesn't exist
+if [ ! -d "$squid_confd" ]; then
+  mkdir -p $squid_confd
+fi
+
 # Generate Random Address
-# Thx to Vladislav V. Prodan [https://gist.github.com/click0/939739]
-# -----
 GenerateAddress() {
   array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
   a=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
@@ -36,10 +31,8 @@ GenerateAddress() {
   echo $network:$a:$b:$c:$d
 }
 
-# -----
 # Run IPv6-Address-Loop
-# -----
-while [ 0=1 ]
+while true
 do
   ip1=$(GenerateAddress)
   echo "[+] add ip1 $ip1"
@@ -56,12 +49,10 @@ tcp_outgoing_address $ip1 ip1" > $squid_confd"01-random-ip1.conf"
   /bin/systemctl reload squid
   sleep $sleeptime
 
-
   ip2=$(GenerateAddress)
   echo "[+] add ip2 $ip2"
 
   $cmd_ip -6 addr add $ip2/64 dev $interface
-  # Set IPv6 in Config
   echo "acl ip2 random 1/2
 tcp_outgoing_address $ip2 ip2" > $squid_confd"02-random-ip2.conf"
 
@@ -73,12 +64,9 @@ tcp_outgoing_address $ip2 ip2" > $squid_confd"02-random-ip2.conf"
   /bin/systemctl reload squid
   sleep $sleeptime
 
-
   ip3=$(GenerateAddress)
   echo "[+] add ip3 $ip3"
   $cmd_ip -6 addr add $ip3/64 dev $interface
-
-  # Set IPv6 in Config
   echo "acl ip3 random 1/1
 tcp_outgoing_address $ip3 ip3" > $squid_confd"03-random-ip3.conf"
 
